b2f2e808a51fd1c5ebfa0d066ca951ba440b7777
Kingsley Ibe <43799727+KingsleyIbe@users.noreply.github.com>
2021-11-25 14:22:07 +0100

    Add Date and Time to the page

--- js/index.js
+++ js/index.js
@@ -1,17 +1,20 @@
+import { DateTime } from '../node_modules/luxon/build/es6/luxon.js';
 import Books from './books.js';
 
 const booksContainer = document.querySelector('.books-container');
 const bookTitle = document.getElementById('title');
 const bookAuthor = document.getElementById('author');
 const addBtn = document.getElementById('add-book');
-
 const bookCollection = new Books(booksContainer);
+
+const now = DateTime.now().toLocaleString(DateTime.DATETIME_MED);
+console.log(now);
 
 function clearInputFields() {
   bookTitle.value = '';
   bookAuthor.value = '';
   bookTitle.focus();
-}
+};
 
 function addBook() {
   const title = bookTitle.value;
